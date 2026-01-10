394765a0b1db5a44c0119fc06767a36360fa4dd7
royalcarriage <info@royalcarriagelimo.com>
2026-01-10 13:32:51 -0600

    Add client/src/components/layout/Layout.tsx

---
+++ client/src/components/layout/Layout.tsx
@@ -0,0 +1,21 @@
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { MobileCTABar } from "./MobileCTABar";
+import { ReactNode } from "react";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1 pb-20 md:pb-0">
+        {children}
+      </main>
+      <Footer />
+      <MobileCTABar />
+    </div>
+  );
+}
