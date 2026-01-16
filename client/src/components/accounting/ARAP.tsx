fd97ac84ae0dc7cc770829d28f79732cedd94403
info <info@royalcarriagelimo.com>
2026-01-16 06:47:04 +0000

    feat: add base accounting UI components

---
+++ client/src/components/accounting/ARAP.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ARAP = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Accounts Receivable / Payable</h2>
+      {/* AR/AP data will be displayed here */}
+      <p>AR/AP details coming soon...</p>
+    </div>
+  );
+};
+
+export default ARAP;
