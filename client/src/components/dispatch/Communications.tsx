d43b0e1a68d8d01b15cb5a737538ce7aae823016
info <info@royalcarriagelimo.com>
2026-01-16 07:00:38 +0000

    feat: add remaining dispatch UI components

---
+++ client/src/components/dispatch/Communications.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Communications = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Communications</h2>
+      {/* Communication logs or features will be displayed here */}
+      <p>Communication details coming soon...</p>
+    </div>
+  );
+};
+
+export default Communications;
