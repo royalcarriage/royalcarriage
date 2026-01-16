008ef436e9e86e8a5751de943f8467733faf8161
info <info@royalcarriagelimo.com>
2026-01-16 06:56:10 +0000

    feat: add base affiliate UI components

---
+++ client/src/components/affiliate/Settlements.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Settlements = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Settlements</h2>
+      {/* Settlement data will be displayed here */}
+      <p>Settlement details coming soon...</p>
+    </div>
+  );
+};
+
+export default Settlements;
