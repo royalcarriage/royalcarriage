3b0a54d6e6c58f078061bd85ed83809250f85835
info <info@royalcarriagelimo.com>
2026-01-16 06:49:21 +0000

    feat: add base fleet UI components

---
+++ client/src/components/fleet/Vehicles.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Vehicles = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Vehicles</h2>
+      {/* Vehicle data will be displayed here */}
+      <p>Vehicle details coming soon...</p>
+    </div>
+  );
+};
+
+export default Vehicles;
