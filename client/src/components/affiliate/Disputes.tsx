c218f09629bb1b5b98b7022c35903001e85e5215
info <info@royalcarriagelimo.com>
2026-01-16 06:56:10 +0000

    feat: add base affiliate UI components

---
+++ client/src/components/affiliate/Disputes.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Disputes = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Disputes</h2>
+      {/* Dispute data will be displayed here */}
+      <p>Dispute details coming soon...</p>
+    </div>
+  );
+};
+
+export default Disputes;
